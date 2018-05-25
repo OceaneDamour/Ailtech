7c8624a5956040683dac74845037ee656b040e1c
OceaneDamour <32702282+OceaneDamour@users.noreply.github.com>
2018-05-25 11:20:06 +0400

    Update login.php

--- model/login.php
+++ model/login.php
@@ -1,8 +1,14 @@
 <?php
- $serveur='localhost';
- $login="root";
-$motdepasse="";
- $mabase="ailtech";
+$serveur='mysql-oceaned.alwaysdata.net';
+ $login="oceaned";
+$motdepasse="root";
+ $mabase="oceaned_ailtech";
+//connexion en local
+// $serveur='localhost';
+ //$login="root";
+//$motdepasse="";
+ //$mabase="ailtech";
+//serveur distant:
 //mysql-oceaned.alwaysdata.net    
  // connexion 
  //nom:oceaned
